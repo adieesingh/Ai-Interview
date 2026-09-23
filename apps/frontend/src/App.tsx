69953826570e7a7baab8e0f1424798de2375e53e
unknown <1606adarsh@gmail.com>
2026-09-23 20:48:49 +0530

    Added toast

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,13 +1,21 @@
-import { Form } from "./components/ui/form";
+import { Form } from "./components/form";
 import "../styles/globals.css";
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Result } from "./components/Result";
+import { Interview } from "./components/Interview";
+import { Toaster } from "sonner";
 
 export function App() {
-  return (
-    <Form></Form>
-     
-  );
+  return <BrowserRouter>
+  <Routes>
+  <Route path="/form" element={<Form></Form>}></Route>
+  <Route path="/result" element={<Result></Result>}></Route>
+  <Route path="/interview" element={<Interview></Interview>}></Route>
+ 
+  </Routes>
+  <Toaster position="bottom-left"></Toaster>
+  </BrowserRouter>
+ 
 }
 
 export default App;
