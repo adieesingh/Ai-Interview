4f7f2c7bdb948272627dff7dbd30298e34cfa478
unknown <1606adarsh@gmail.com>
2026-09-25 12:21:53 +0530

    Added

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Form } from "./components/form";
+import { Form } from "./components/Form";
 import "../styles/globals.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Result } from "./components/Result";
